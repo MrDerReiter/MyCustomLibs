624dab769e9c64c6651d5b501f25eaa97e57af70
DerReiter <riderbcrich2@gmail.com>
2024-08-15 23:24:44 +0300

    MVVMToolkit Версия 1.1.0 В библиотеку добавлены вспомогательные методы расширения. Добавлен метод расширения AddRange для коллекции BindingList из нативной библиотеки .NET

---
+++ MVVMToolkit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace MVVMToolkit
+{
+    public static class Extensions
+    {
+        public static void AddRange<T>(this BindingList<T> list, IEnumerable<T> range)
+        {
+            foreach (var item in range)
+                list.Add(item);
+        }
+    }
+}
