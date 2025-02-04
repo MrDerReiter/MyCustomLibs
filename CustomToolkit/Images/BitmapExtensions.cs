6485b31d48d6f6c940d6f1fee5db66ae2a45b55d
DerReiter <riderbcrich2@gmail.com>
2025-02-04 18:07:21 +0300

    CustomToolkit Версия 1.3.0 Добавлено пространство имён Images и класс ImageWrapper для работы с изображениями.

---
+++ CustomToolkit/Images/BitmapExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace CustomToolkit.Images
+{
+    /// <summary>
+    /// Методы расширения для класса Bitmap и массивов с данными объектами.
+    /// </summary>
+    public static class BitmapExtensions
+    {
+        private static void CheckDir(string dir)
+        {
+            if(!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+        }
+
+
+        /// <summary>
+        /// Сохраняет все изображения последовательности в указанном каталоге 
+        /// (если не существует, будет создан);
+        /// в формате JPG, каждое в отдельный файл.
+        /// Файлы будут названы image1, image2 и т.д., по количеству
+        /// битмапов в исходной последовательности.
+        /// </summary>
+        /// <param name="sourcePicts"></param>
+        /// <param name="folderPath"></param>
+        public static void SaveJPG(this IEnumerable<Bitmap> sourcePicts, string folderPath)
+        {
+            CheckDir(folderPath);
+
+            int index = 1;
+            foreach (var pict in sourcePicts)
+                pict.Save($"{folderPath}\\image{index++}.jpg");
+        }
+
+        /// <summary>
+        /// Сохраняет все изображения последовательности в указанном каталоге 
+        /// (если не существует, будет создан);
+        /// в формате PNG, каждое в отдельный файл.
+        /// Файлы будут названы image1, image2 и т.д., по количеству
+        /// битмапов в исходной последовательности.
+        /// </summary>
+        /// <param name="sourcePicts"></param>
+        /// <param name="folderPath"></param>
+        public static void SavePNG(this IEnumerable<Bitmap> sourcePicts, string folderPath)
+        {
+            CheckDir(folderPath);
+
+            int index = 1;
+            foreach (var pict in sourcePicts)
+                pict.Save($"{folderPath}\\image{index++}.png");
+        }
+
+        /// <summary>
+        /// Возвращает поток, содержащий это изображение в его исходном формате.
+        /// </summary>
+        /// <param name="pict"></param>
+        /// <returns></returns>
+        public static Stream ToStream(this Bitmap pict)
+        {
+            var format = pict.RawFormat;
+            var stream = new MemoryStream();
+
+            pict.Save(stream, format);
+            return stream;
+        }
+    }
+}
---
+++ CustomToolkit/Images/ImageWrapper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace CustomToolkit.Images
+{
+    /// <summary>
+    /// Статический класс для работы с изображениями.
+    /// Предоставляет методы для выполнения часто используемых шаблонных операций.
+    /// </summary>
+    public static class ImageWrapper
+    {
+        /// <summary>
+        /// Разрезает изображение на указанное количество 
+        /// частей по горизонтали и возвращает массив с этими изображениями.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="chunks"></param>
+        /// <returns></returns>
+        internal static Bitmap[] SliceH(Bitmap source, int chunks)
+        {
+            var result = new Bitmap[chunks];
+            int targetHeight = source.Height / chunks;
+
+            for (int i = 0; i < chunks; i++)
+            {
+                var target = new Bitmap(source.Width, targetHeight);
+                var targetRect = new Rectangle(new Point(0, 0), target.Size);
+                var cropRect = new Rectangle
+                    (new Point(0, targetHeight * i), target.Size);
+
+                Graphics.FromImage(target)
+                        .DrawImage(source, targetRect, cropRect, GraphicsUnit.Pixel);
+                result[i] = target;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Разрезает изображение на указанное количество 
+        /// частей по вертикали и возвращает массив с этими изображениями.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="chunks"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        internal static Bitmap[] SliceV(Bitmap source, int chunks)
+        {
+            var result = new Bitmap[chunks];
+            int targetWidth = source.Width / chunks;
+
+            for (int i = 0; i < chunks; i++)
+            {
+                var target = new Bitmap(targetWidth, source.Height);
+                var targetRect = new Rectangle(new Point(0, 0), target.Size);
+                var cropRect = new Rectangle
+                    (new Point(targetWidth * i, 0), target.Size);
+
+                Graphics.FromImage(target)
+                        .DrawImage(source, targetRect, cropRect, GraphicsUnit.Pixel);
+                result[i] = target;
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Разрезает изображение из указанного файла на равные по ширине и высоте части
+        /// (по условной "сетке") и возвращает массив объектов Bitmap с этими изображениями.
+        /// Параметры chunksH и chunksV определяют размеры условной сетки, количество рассечений 
+        /// исходного изображения по горизонатали (ряды) и вертикали (колонки) соответственно 
+        /// (1 - без рассечения в данной плоскости, 2 - рассечение пополам и т.д.; 
+        /// если оба параметра равны 1, вернётся массив с одним исходным изображением;
+        /// ноль и отрицательный значения недопустимы).
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="chunksH"></param>
+        /// <param name="chunksV"></param>
+        /// <returns></returns>
+        public static Bitmap[] Slice(string filePath, int chunksH, int chunksV)
+        {
+            if (chunksH <= 0 || chunksV <= 0)
+                throw new ArgumentException
+                    ("Значения параметров chunksH/chunksV должны быть не меньше чем 1");
+
+            int outCount = chunksH * chunksV;
+            var result = new Bitmap[outCount];
+            var source = new Bitmap(filePath);
+
+            var index = 0;
+            var partsH = SliceH(source, chunksH);
+            for (int i = 0; i < chunksH; i++)
+            {
+                var partsV = SliceV(partsH[i], chunksV);
+                for (int j = 0; j < chunksV; j++)
+                {
+                    result[index++] = partsV[j];
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Перегрузка метода для нарезки исходного избражения с получением
+        /// выходных изображений разной ширины.
+        /// Принимает в качестве второго аргумента последовательность/массив чисел.
+        /// Изображение будет рассечено по горизонтали (ряды) в соответствии с количеством чисел
+        /// в последовательности, после чего для каждого числа каждая часть 
+        /// будет рассечена по вертикали на соответствующее этому числу количество частей.
+        /// Последовательность не должна содержать нулей или отрицательных значений. 
+        /// (Значение 1 допустимо, в этом случае фрагмент не будет рассечён по вертикали 
+        /// и будет добавлен в выходной массив в исходном виде).
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="chunks"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static Bitmap[] Slice(string filePath, params int[] chunks)
+        {
+            foreach (var number in chunks)
+                if (number <= 0) throw new ArgumentException
+                        ("Значения параметров нарезки должны быть не меньше чем 1");
+
+            int outCount = chunks.Sum();
+            var result = new Bitmap[outCount];
+            var source = new Bitmap(filePath);
+
+            var index = 0;
+            var partsH = SliceH(source, chunks.Length);
+            for (int i = 0; i < partsH.Length; i++)
+            {
+                var partsV = SliceV(partsH[i], chunks[i]);
+                for (int j = 0; j < partsV.Length; j++)
+                {
+                    result[index++] = partsV[j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
